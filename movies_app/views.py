a7c08258e629f50127766eb7ebe769c7cfa30a7a
xaxich <148545213+xaxich@users.noreply.github.com>
2026-01-16 18:43:12 +1000

    Update views.py
    add vies

--- movies_app/views.py
+++ movies_app/views.py
@@ -5,6 +5,41 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie
 from .forms import MovieForm
 
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_list.jinja', {'movies': movies})
+
+
+def movie_create(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('movie_list')
+    else:
+        form = MovieForm()
+    return render(request, 'movie_form.jinja', {'form': form})
+
+
+def movie_update(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == 'POST':
+        form = MovieForm(request.POST, instance=movie)
+        if form.is_valid():
+            form.save()
+            return redirect('movie_list')
+    else:
+        form = MovieForm(instance=movie)
+    return render(request, 'movie_form.jinja', {'form': form})
+
+
+def movie_delete(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == 'POST':
+        movie.delete()
+        return redirect('movie_list')
+    return render(request, 'movie_confirm_delete.jinja', {'movie': movie})
+
 
 def index(request):
     # Основные данные
